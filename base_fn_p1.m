0efcc8746cf9552221833c6e165d3a0dc58a5f18
arybin <arybin@163.com>
2017-06-03 11:06:29 +0800

    added base_fn_p1

---
+++ base_fn_p1.m
@@ -0,0 +1,11 @@
+function [U,UU]=base_fn_p1(a,b,h)
+U={};UU={};
+X=a:h:b;
+len=length(X);
+for i=2:len-1
+  U{i-1}=@(x) (x>=X(i-1)&x<=X(i))*(x-X(i-1))/h+...
+              (x>X(i)&x<=X(i+1))*(X(i+1)-x)/h;
+
+  UU{i-1}=@(x) (x>=X(i-1)&x<=X(i))/h+...
+               (x>X(i)&x<=X(i+1))/(-h);
+end
--- fn_inner_product_builder.m
+++ fn_inner_product_builder.m
@@ -1,2 +1,2 @@
 function fn=fn_inner_product_builder(a,b)
-fn=@(fn_a,fn_b) quad(fn_multiply(fn_a,fn_b),a,b);
+fn=@(fn_a,fn_b) quadgk(fn_multiply(fn_a,fn_b),a,b);
