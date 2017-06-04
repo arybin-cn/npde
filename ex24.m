763c42ed6e29e6cf58dd84fe0caca2fbc7cc0440
arybin <arybin@163.com>
2017-06-04 14:16:32 +0800

    refined ex25 with fem framework

--- ex24.m
+++ ex24.m
@@ -1,42 +1,32 @@
 format long;
-h=0.02;
-A=[];F=[];C=[];
-ep=0.1;beta=5;
-figure;
-[U,V]=base_fn_p1(0,1,h);
-dimension=length(U);
+a=0;b=1;ep=0.1;beta=5;
 
-fn_ep=@(x) ep;
-fn_beta=@(x) beta;
+ip2=fn_inner_product_builder(a,b,2);
+ip1=fn_inner_product_builder(a,b,1);
 
-p=fn_inner_product_builder(0,1);
-for i=1:dimension
-    for j=1:dimension
-      A(i,j)=p(fn_multiply(fn_ep,V{i}),V{j})+...
-      p(fn_multiply(fn_beta,V{j}),U{i});
-  end
+stf_matrix_builder=@(i,j,U,dU) ep*ip2(dU{i},dU{j})+beta*ip2(U{i},dU{j});
+rhs_vector_builder=@(i,U,dU) -beta*ip1(U{i});
+
+base_builder_p1=base_fn_builder(a,b,@base_fn_p1);
+base_builder_p2=base_fn_builder(a,b,@base_fn_p2);
+
+for k=1:4
+  h=0.14/(2*k-1);
+  [C_p1,U_p1]=fem_framework(stf_matrix_builder,...
+  rhs_vector_builder,base_builder_p1,h);
+  [C_p2,U_p2]=fem_framework(stf_matrix_builder,...
+  rhs_vector_builder,base_builder_p2,h);
+
+  X=a:0.005:b;
+  Y_p1=fem_compute(C_p1,U_p1,X);
+  Y_p2=fem_compute(C_p2,U_p2,X);
+  subplot(4,2,(k-1)*2+1);
+  scatter(X,Y_p1,'*','r');
+  title(sprintf('P1-FEM with h=%.3f',h));
+  subplot(4,2,(k-1)*2+2);
+  scatter(X,Y_p2,'p','g');
+  title(sprintf('P2-FEM with h=%.3f',h));
 end
 
-for j=1:dimension
-  F(j)=-p(fn_beta,U{j});
-end
-
-C=A\F';
-
-fn_exact=@(x) (exp(beta*x/ep)-1)./(exp(beta/ep)-1);
-X=0:0.001:1;
-Y_EXACT=fn_exact(X);
-Y_FEM=[];
-for i=1:length(X)
-  Y_FEM(i)=X(i);
-  for j=1:dimension
-    Y_FEM(i)=Y_FEM(i)+U{j}(X(i))*C(j);
-  end
-end
-plot(X,Y_EXACT,'g');hold on;
-scatter(X,Y_EXACT,'o','g');hold on;
-plot(X,Y_FEM,'r');hold on;
-scatter(X,Y_FEM,'*','r');hold on;
-norm(Y_EXACT-Y_FEM,2)
-
-saveas(gcf,'ex24.png');
+set(gcf,'PaperUnits','Inches','PaperPosition',[0 0 10 20]);
+print('-djpeg','-r100','ex24');
--- ex25.m
+++ ex25.m
@@ -1,32 +1,33 @@
 format long;
-figure;
-for k=1:4
-a=0;b=1;h=0.2/k;
+a=0;b=1;fn_SS=@(x) (9/4)*x-3/2;
+fn_S36=@(x) ((1-(3/2)*x+(9/8)*x.^2).^3)/6;
+
 ip3=fn_inner_product_builder(a,b,3);
 ip2=fn_inner_product_builder(a,b,2);
 
-fn_S36=@(x) ((1-(3/2)*x+(9/8)*x.^2).^3)/6;
-fn_SS=@(x) (9/4)*x-3/2;
-
-base_builder_p1=base_fn_builder(a,b,@base_fn_p1);
-base_builder_p2=base_fn_builder(a,b,@base_fn_p2);
 stf_matrix_builder=@(i,j,U,dU) ip3(fn_S36,dU{j},dU{i});
 rhs_vector_builder=@(i,U,dU) -ip2(fn_SS,U{i});
 
-[C_p1,U_p1]=fem_framework(stf_matrix_builder,...
-            rhs_vector_builder,base_builder_p1,h);
-[C_p2,U_p2]=fem_framework(stf_matrix_builder,...
-            rhs_vector_builder,base_builder_p2,h);
+base_builder_p1=base_fn_builder(a,b,@base_fn_p1);
+base_builder_p2=base_fn_builder(a,b,@base_fn_p2);
 
-X=a:0.01:b;
-Y_p1=fem_compute(C_p1,U_p1,X);
-Y_p2=fem_compute(C_p2,U_p2,X);
-subplot(4,2,(k-1)*2+1);
-scatter(X,Y_p1,'*','r');
-title(sprintf('P1-FEM with h=%.3f',h));
-subplot(4,2,(k-1)*2+2);
-scatter(X,Y_p2,'p','g');
-title(sprintf('P2-FEM with h=%.3f',h));
+for k=1:4
+  h=0.2/k;
+  [C_p1,U_p1]=fem_framework(stf_matrix_builder,...
+  rhs_vector_builder,base_builder_p1,h);
+  [C_p2,U_p2]=fem_framework(stf_matrix_builder,...
+  rhs_vector_builder,base_builder_p2,h);
+
+  X=a:0.01:b;
+  Y_p1=fem_compute(C_p1,U_p1,X);
+  Y_p2=fem_compute(C_p2,U_p2,X);
+  subplot(4,2,(k-1)*2+1);
+  scatter(X,Y_p1,'*','r');
+  title(sprintf('P1-FEM with h=%.3f',h));
+  subplot(4,2,(k-1)*2+2);
+  scatter(X,Y_p2,'p','g');
+  title(sprintf('P2-FEM with h=%.3f',h));
 end
+
 set(gcf,'PaperUnits','Inches','PaperPosition',[0 0 10 20]);
 print('-djpeg','-r100','ex25');
